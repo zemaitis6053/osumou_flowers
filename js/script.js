c129785cc6164d224b792b955454b2f01cf3e06e
zemaitis6053 <80103915+zemaitis6053@users.noreply.github.com>
2021-08-25 21:50:49 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -222,7 +222,7 @@ $(window).on('load', function () {
 // ページ内リンク
 $('#g-nav a[href*="#"]').click(function () {
 	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-	var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+	var pos = $(elmHash).offset().top-100;	//idの上部の距離を取得
 	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
 	return false;
 });
