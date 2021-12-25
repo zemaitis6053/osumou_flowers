2d3d903a021ba90bec018c55da8458301f2a2a9a
zemaitis6053 <80103915+zemaitis6053@users.noreply.github.com>
2021-12-25 12:59:01 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 /*===========================================================*/
-/*機能編 5-1-3 ドロップダウンメニュー（写真付 上ナビ）*/
+/* ドロップダウンメニュー（写真付 上ナビ）*/
 /*===========================================================*/
-//ドロップダウンの設定を関数でまとめる
 function mediaQueriesWin(){
-	var width = $(window).width();
+	const width = $(window).width();
 	if(width <= 850) {//横幅が850px以下の場合
 		$(".has-child>a").off('click');	//has-childクラスがついたaタグのonイベントを複数登録を避ける為offにして一旦初期状態へ
-		$(".has-child>a").on('click', function() {//has-childクラスがついたaタグをクリックしたら
-			var parentElem =  $(this).parent();// aタグから見た親要素の<li>を取得し
+		$(".has-child>a").on('click', function() {
+			const parentElem =  $(this).parent();// aタグから見た親要素の<li>を取得し
 			$(parentElem).toggleClass('active');//矢印方向を変えるためのクラス名を付与して
 			$(parentElem).children('ul').stop().slideToggle(500);//liの子要素のスライドを開閉させる※数字が大きくなるほどゆっくり開く
 			return false;//リンクの無効化
@@ -20,60 +19,44 @@ function mediaQueriesWin(){
 }
 
 /*===========================================================*/
-/*機能編 5-1-10スクロールすると位置が固定して追従*/
+/* クリックしたら円形背景が拡大（左上から）*/
 /*===========================================================*/
-
-// $(window).on('load resize', function() {
-// 	var windowWidth = window.innerWidth;
-// 	var elements = $('.sub-fix-block');//position: sticky;を指定している要素
-// 	if (windowWidth >= 1020) {/*1020px以上にIE用のJSをきかせる*/
-// 		Stickyfill.add(elements);
-// 	}else{
-// 		Stickyfill.remove(elements);
-// 	} 
-// });
-
-/*===========================================================*/
-/*機能編 5-1-22 クリックしたら円形背景が拡大（左上から）*/
-/*===========================================================*/
-
-$(".openbtn").click(function () {//ボタンがクリックされたら
-	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+$(".openbtn").click(function () {
+	$(this).toggleClass('active');
+    $("#g-nav").toggleClass('panelactive');
+    $(".circle-bg").toggleClass('circleactive');
 });
 
-$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
-    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
-    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+$("#g-nav a").click(function () {
+    $(".openbtn").removeClass('active');
+    $("#g-nav").removeClass('panelactive');
+    $(".circle-bg").removeClass('circleactive');
 });
 
 /*===========================================================*/
-/*機能編 8-1-2 ページの指定の高さを超えたら下から出現*/
+/*ページの指定の高さを超えたら下から出現*/
 /*===========================================================*/
-//スクロールした際の動きを関数でまとめる
+//スクロールした際の動き
 function PageTopAnime() {
-
-	var scroll = $(window).scrollTop(); //スクロール値を取得
-	if (scroll >= 200){//200pxスクロールしたら
-		$('#page-top').removeClass('DownMove');		// DownMoveというクラス名を除去して
-		$('#page-top').addClass('UpMove');			// UpMoveというクラス名を追加して出現
-	}else{//それ以外は
-		if($('#page-top').hasClass('UpMove')){//UpMoveというクラス名が既に付与されていたら
-			$('#page-top').removeClass('UpMove');	//  UpMoveというクラス名を除去し
-			$('#page-top').addClass('DownMove');	// DownMoveというクラス名を追加して非表示
+	const scroll = $(window).scrollTop(); //スクロール値を取得
+	if (scroll >= 200){
+		$('#page-top').removeClass('DownMove');
+		$('#page-top').addClass('UpMove');
+	}else{
+		if($('#page-top').hasClass('UpMove')){
+			$('#page-top').removeClass('UpMove');
+			$('#page-top').addClass('DownMove');
 		}
 	}
 	
-	var wH = window.innerHeight; //画面の高さを取得
-	var footerPos =  $('#footer').offset().top; //footerの位置を取得
+	const wH = window.innerHeight; //画面の高さを取得
+	const footerPos =  $('#footer').offset().top; //footerの位置を取得
 	if(scroll+wH >= (footerPos+10)) {
-		var pos = (scroll+wH) - footerPos+10 //スクロールの値＋画面の高さからfooterの位置＋10pxを引いた場所を取得し
-		$('#page-top').css('bottom',pos);	//#page-topに上記の値をCSSのbottomに直接指定してフッター手前で止まるようにする
+		var pos = (scroll+wH) - footerPos+10
+		$('#page-top').css('bottom',pos);	//#page-topに上記の値をCSSのbottomに直接指定してフッター手前で止まる
 	}else{//それ以外は
-		if($('#page-top').hasClass('UpMove')){//UpMoveというクラス名がついていたら
-			$('#page-top').css('bottom','10px');// 下から10pxの位置にページリンクを指定
+		if($('#page-top').hasClass('UpMove')){
+			$('#page-top').css('bottom','10px');
 		}
 	}
 }
@@ -81,27 +64,24 @@ function PageTopAnime() {
 // #page-topをクリックした際の設定
 $('#page-top').click(function () {
     $('body,html').animate({
-        scrollTop: 0//ページトップまでスクロール
-    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
-    return false;//リンク自体の無効化
+        scrollTop: 0
+    }, 500);
+    return false;
 });
 
 /*===========================================================*/
-/*機能編 9-3-1ツールチップ*/
+/* ツールチップ*/
 /*===========================================================*/
-
-tippy('.cap', {//指定した要素にツールチップが出現
-	placement: 'top-start',//ツールチップの表示位置⇒top、top-start、top-end、right、right-start、right-end、bottom、bottom-start、bottom-end、left、left-start、left-end。指定をしなくてもtopに表示
-	animation: 'shift-toward-subtle',//ツールチップ出現の動き。動きを指定するにはhttps://unpkg.com/browse/tippy.js@5.0.3/animations/から任意の動きを選び<head>内に読み込むことが必要。使用できる動き⇒shift-away、shift-away-subtle、shift-away-extreme、shift-toward、shift-toward-subtle、shift-toward-extreme、scale、scale-subtle、scale-extreme、perspective、perspective-subtle、perspective-extreme。指定をしなくてもfadeで表示
-	//theme: 'light-border',//ツールチップのテーマの色。色を指定するにはhttps://unpkg.com/browse/tippy.js@5.0.3/themes/からテーマを選び<head>内に読み込んで指定する。テーマの種類⇒light、light-border、material、translucent。指定をしなくても黒色で表示
-	duration: 200,//ツールチップの出現の速さをミリ秒単位で指定
+tippy('.cap', {
+	placement: 'top-start',
+	animation: 'shift-toward-subtle',
+	duration: 200,
 }
 )
 
 /*===========================================================*/
-/*機能編 6-1-7	複数画像を中央に注目させて見せる*/
+/* Slick_Slider */
 /*===========================================================*/
-
 $('.slider').slick({
 		autoplay: true,//自動的に動き出すか。初期値はfalse。
 		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
@@ -110,21 +90,19 @@ $('.slider').slick({
 		slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
 		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
 		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-		centerMode: true,//要素を中央ぞろえにする
+		centerMode: true,//要素を中央ぞろえ
 		variableWidth: true,//幅の違う画像の高さを揃えて表示
 		dots: true,//下部ドットナビゲーションの表示
 	});
 
-
 /*===========================================================*/
-/*印象編 4-12 順番に現れる（CSS x jQuery）*/
+/* 順番に現れる（CSS x jQuery）*/
 /*===========================================================*/
-
 function delayScrollAnime() {
 	var time = 0.2;//遅延時間を増やす秒数の値
 	var value = time;
 	$('.delayScroll').each(function () {
-		var parent = this;					//親要素を取得
+		var parent = this;//親要素を取得
 		var elemPos = $(this).offset().top;//要素の位置まで来たら
 		var scroll = $(window).scrollTop();//スクロール値を取得
 		var windowHeight = $(window).height();//画面の高さを取得
@@ -132,13 +110,11 @@ function delayScrollAnime() {
 		
 		if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
 			$(childs).each(function () {
-				
 				if (!$(this).hasClass("flipRight")) {//アニメーションのクラス名が指定されているかどうかをチェック
-					
 					$(parent).addClass("play");	//親要素にクラス名playを追加
-					$(this).css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
-					$(this).addClass("flipRight");//アニメーションのクラス名を追加
-					value = value + time;//delay時間を増加させる
+					$(this).css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加
+					$(this).addClass("flipRight");
+					value = value + time;//delay時間を増加
 					
 					//全ての処理を終わったらplayを外す
 					var index = $(childs).index(this);
@@ -148,96 +124,90 @@ function delayScrollAnime() {
 				}
 			})
 		}else {
-			$(childs).removeClass("flipRight");//アニメーションのクラス名を削除
+			$(childs).removeClass("flipRight");
 			value = time;//delay初期値の数値に戻す
 		}
 	})
 }
 /*===========================================================*/
-/* 印象編 4 最低限おぼえておきたい動き*/
+/* 要素の動き */
 /*===========================================================*/
-
-// 動きのきっかけの起点となるアニメーションの名前を定義
 function fadeAnime(){
-
-	//4-1 ふわっ（下から）
-    
-$('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
-		var elemPos = $(this).offset().top-50;//要素より、50px上の
-		var scroll = $(window).scrollTop();
-		var windowHeight = $(window).height();
+	//下から
+$('.fadeUpTrigger').each(function(){
+		const elemPos = $(this).offset().top-50;
+		const scroll = $(window).scrollTop();
+		const windowHeight = $(window).height();
 		if (scroll >= elemPos - windowHeight){
-		$(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
-		}else{
-		$(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
-		}
+			$(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+			}else{
+			$(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+			}
 		});
 
-	//4-1 ふわっ（上から）
-
-$('.fadeDownTrigger').each(function(){ //fadeDownTriggerというクラス名が
-		var elemPos = $(this).offset().top-50;//要素より、50px上の
-		var scroll = $(window).scrollTop();
-		var windowHeight = $(window).height();
+	//上から
+$('.fadeDownTrigger').each(function(){
+		const elemPos = $(this).offset().top-50;
+		const scroll = $(window).scrollTop();
+		const windowHeight = $(window).height();
 		if (scroll >= elemPos - windowHeight){
-		$(this).addClass('fadeDown');// 画面内に入ったらfadeDownというクラス名を追記
-		}else{
-		$(this).removeClass('fadeDown');// 画面外に出たらfadeDownというクラス名を外す
-		}
+			$(this).addClass('fadeDown');
+			}else{
+			$(this).removeClass('fadeDown');
+			}
 		});
 }
 
 /*===========================================================*/
-/* 関数をまとめる*/
+/* 関数まとめ */
 /*===========================================================*/
-
 // ページがリサイズされたら動かしたい場合の記述
 $(window).resize(function() {
-	mediaQueriesWin();/*機能編  5-1-3 ドロップダウンメニュー（写真付 上ナビ）の関数を呼ぶ*/
+	mediaQueriesWin();/* ドロップダウンメニュー（写真付 上ナビ）*/
 });
 
-// 画面をスクロールをしたら動かしたい場合の記述
+// 画面をスクロールで動作
 $(window).scroll(function () {
-	PageTopAnime();/*機能編  8-1-2 ページの指定の高さを超えたら下から出現の関数を呼ぶ*/
-    fadeAnime();/* 印象編 4 最低限おぼえておきたい動き*/
-	delayScrollAnime();/* 印象編 4-12 順番に現れる（CSS x jQuery）関数を呼ぶ*/
+	PageTopAnime();
+    fadeAnime();
+	delayScrollAnime();
 });
 
-// ページが読み込まれたらすぐに動かしたい場合の記述
+// ページが読み込まれた動作
 $(window).on('load', function () {
-	mediaQueriesWin();/*機能編  5-1-3 ドロップダウンメニュー（写真付 上ナビ）の関数を呼ぶ*/
-	PageTopAnime();/*機能編  8-1-2 ページの指定の高さを超えたら下から出現の関数を呼ぶ*/
-    fadeAnime();/* 印象編 4 最低限おぼえておきたい動き*/
-	delayScrollAnime();/* 印象編 4-12 順番に現れる（CSS x jQuery）関数を呼ぶ*/
+	mediaQueriesWin();/* ドロップダウンメニュー（写真付 上ナビ）*/
+	PageTopAnime();
+    fadeAnime();
+	delayScrollAnime();
 
-    /*機能編 9-2-1 任意の場所をクリックすると隠れていた内容が開くの読み込み*/
-	$(".open").each(function(index, element){	//openクラスを取得
-		var Title =$(element).children('.title');	//openクラスの子要素のtitleクラスを取得
-		$(Title).addClass('close');				//タイトルにクラス名closeを付与し
-		var Box =$(element).children('.box');	//openクラスの子要素boxクラスを取得
-		$(Box).slideDown(500);					//アコーディオンを開く
+	// アコーディオン 
+	$(".open").each(function(index, element){
+		const Title =$(element).children('.title');
+		$(Title).addClass('close');
+		const Box =$(element).children('.box');
+		$(Box).slideDown(500);
 	});
 });
 
 // ページ内リンク
 $('#g-nav a[href*="#"]').click(function () {
-	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-	var pos = $(elmHash).offset().top-100;	//idの上部の距離を取得
-	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	const elmHash = $(this).attr('href');
+	const pos = $(elmHash).offset().top-100;
+	$('body,html').animate({scrollTop: pos}, 500);
 	return false;
 });
 
 // ページ外リンク
 $(window).on('load', function() {
-	var headerHight = 100; //ヘッダの高さ
+	const headerHight = 100;
 	if(document.URL.match("#")) {
-	var str = location.href ;
-	var cut_str = "#";
-	var index = str.indexOf(cut_str);
-	var href = str.slice(index);
-	var target = href;
-	var position = $(target).offset().top - headerHight;
-	$("html, body").animate({scrollTop:position}, 500,"swing");
-	return false;
-}
+		const str = location.href ;
+		const cut_str = "#";
+		const index = str.indexOf(cut_str);
+		const href = str.slice(index);
+		const target = href;
+		const position = $(target).offset().top - headerHight;
+		$("html, body").animate({scrollTop:position}, 500,"swing");
+		return false;
+	}
 });
