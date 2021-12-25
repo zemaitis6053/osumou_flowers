8afee4e3ad69b4e4af0240497bd97caa667e3ab4
zemaitis6053 <80103915+zemaitis6053@users.noreply.github.com>
2021-12-25 17:17:18 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -85,7 +85,7 @@ tippy('.cap', {
 $('.slider').slick({
 		autoplay: true,//自動的に動き出すか。初期値はfalse。
 		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-		speed: 500,//スライドのスピード。初期値は300。
+		speed: 1500,//スライドのスピード。初期値は300。
 		slidesToShow: 3,//スライドを画面に3枚見せる
 		slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
 		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
@@ -93,6 +93,8 @@ $('.slider').slick({
 		centerMode: true,//要素を中央ぞろえ
 		variableWidth: true,//幅の違う画像の高さを揃えて表示
 		dots: true,//下部ドットナビゲーションの表示
+		autoplaySpeed: 1500, //自動再生時のスライド切り替えのスピード
+    pauseOnFocus: false, //フォーカスしても自動再生が再開
 	});
 
 /*===========================================================*/
